acbf9615dc5dbe485a755741a3c7e665ea907517
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-03-14 21:49:32 +0600

    day 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'recipe.dart';
 
 void main() {
   runApp(const RecipesApp());
@@ -41,9 +42,52 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SafeArea(
         // TODO: Replace child: Container()
         // 4
-        child: Container(),
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (context, int index) {
+            // TODO: Add GestureDetector
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Text('Detailed Page');
+                    },
+                  ),
+                );
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+          },
+        ),
       ),
     );
   }
+
 // TODO: Add buildRecipeCard() here
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'palatino'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
