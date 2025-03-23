eaad1fd1d23fd5e56cf6e7826c7d7d110458d03a
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-03-24 00:55:32 +0600

    yummy day 04

---
+++ lib/color_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class ColorButton extends StatelessWidget {
+  final void Function(int) changeColor;
+  final ColorSelection colorSelected;
+  const ColorButton(
+      {super.key, required this.changeColor, required this.colorSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      icon: Icon(
+        Icons.opacity_outlined,
+        color: Theme.of(context).colorScheme.onSurfaceVariant,
+      ),
+      itemBuilder: (context) {
+        return List.generate(
+          // we use List.generate for static and few dataset
+          ColorSelection.values.length,
+          (index) {
+            final currentColor = ColorSelection.values[index];
+            return PopupMenuItem(
+              value: index,
+              enabled: currentColor != colorSelected,
+              child: Wrap(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Icon(
+                      Icons.opacity_outlined,
+                      color: currentColor.color,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(currentColor.label),
+                  )
+                ],
+              ),
+            );
+          },
+        );
+      },
+      onSelected: changeColor,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'color_button.dart';
+import 'theme_button.dart';
 
 void main() {
   runApp(Yummy());
 }
 
-class Yummy extends StatelessWidget {
+class Yummy extends StatefulWidget {
   // TODO: Setup default theme
   // 2
   Yummy({super.key});
+
+  @override
+  State<Yummy> createState() => _YummyState();
+}
+
+class _YummyState extends State<Yummy> {
+  ThemeMode themeMode = ThemeMode.light; //Manual theme toggle
+  ColorSelection colorselected = ColorSelection.pink;
+
   // TODO: Add changeTheme above here
+  void changeThemeMode(bool useLightMode) {
+    setState(() {
+      themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
+  void changeColor(int value) {
+    setState(() {
+      colorselected = ColorSelection.values[value];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     const appTitle = 'Yummy';
     // TODO: Setup default theme
-    ThemeMode themeMode = ThemeMode.light; //Manual theme toggle
-    ColorSelection colourselected = ColorSelection.pink;
+
     // 3
     return MaterialApp(
       title: appTitle,
@@ -24,19 +45,23 @@ class Yummy extends StatelessWidget {
       // TODO: Add theme
       themeMode: themeMode, //that uppoer themeMode variable
       theme: ThemeData(
-          colorSchemeSeed: colourselected.color,
+          colorSchemeSeed: colorselected.color,
           useMaterial3: true,
           brightness: Brightness.light),
       darkTheme: ThemeData(
-          colorSchemeSeed: colourselected.color,
+          colorSchemeSeed: colorselected.color,
           useMaterial3: true,
           brightness: Brightness.dark),
       // TODO: Replace Scaffold with Home widget
       // 4
       home: Scaffold(
         appBar: AppBar(
-          surfaceTintColor: colourselected.color,
+          surfaceTintColor: colorselected.color,
           // TODO: Add action buttons
+          actions: [
+            ThemeButton(changeThemeMode: changeThemeMode),
+            ColorButton(changeColor: changeColor, colorSelected: colorselected)
+          ],
           elevation: 4.0,
           title: const Text(
             appTitle,
