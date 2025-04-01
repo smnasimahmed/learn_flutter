e8c74559437d2b791b80c329347827d6e76cfe4d
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-04-02 00:01:46 +0600

    yummy day 09

---
+++ lib/components/post_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../models/post.dart';
+
+class PostCard extends StatelessWidget {
+  final Post post;
+  const PostCard({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context)
+        .textTheme
+        .apply(displayColor: Theme.of(context).colorScheme.onSurface);
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // TODO: Add CircleAvatar
+            // TODO: Add spacing
+            // TODO: Add Expanded Widget
+          ],
+        ),
+      ),
+    );
+  }
+}
