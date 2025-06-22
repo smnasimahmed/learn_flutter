0ac4bb1e7ba3b84f5d29ca1e4f2b58c15b3ed84e
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-06-22 21:49:41 +0600

    routing 2.0 init

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'constants.dart';
+// import 'home.dart';
+import '../models/models.dart';
+import 'screens/screens.dart';
+
+void main() {
+  runApp(const Yummy());
+}
+
+/// Allows the ability to scroll by dragging with touch, mouse, and trackpad.
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+    PointerDeviceKind.touch,
+    PointerDeviceKind.mouse,
+    PointerDeviceKind.trackpad,
+  };
+}
+
+class Yummy extends StatefulWidget {
+  const Yummy({super.key});
+
+  @override
+  State<Yummy> createState() => _YummyState();
+}
+
+class _YummyState extends State<Yummy> {
+  ThemeMode themeMode = ThemeMode.light;
+  ColorSelection colorSelected = ColorSelection.pink;
+
+  /// Authentication to manage user login session
+  // ignore: unused_field
+  final YummyAuth _auth = YummyAuth();
+
+  /// Manage user's shopping cart for the items they order.
+  final CartManager _cartManager = CartManager();
+
+  /// Manage user's orders submitted
+  final OrderManager _orderManager = OrderManager();
+
+  // TODO: Initialize GoRouter
+
+  // TODO: Add Redirect Handler
+
+  void changeThemeMode(bool useLightMode) {
+    setState(() {
+      themeMode = useLightMode
+          ? ThemeMode
+                .light //
+          : ThemeMode.dark;
+    });
+  }
+
+  void changeColor(int value) {
+    setState(() {
+      colorSelected = ColorSelection.values[value];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace with Router
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Uncomment to remove Debug banner
+      scrollBehavior: CustomScrollBehavior(),
+      themeMode: themeMode,
+      theme: ThemeData(
+        colorSchemeSeed: colorSelected.color,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: colorSelected.color,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: LoginPage(onLogIn: (credentials) {}),
+    );
+  }
+}
